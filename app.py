83561432d75fb76173df452a4000758358bc6a2f
Azdine Bahloul <125593795+azdinebahloul@users.noreply.github.com>
2023-04-10 16:59:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ st.markdown("<p style='text-align: justify; font-size: 18px; font-family: Arial,
 import streamlit as st
 from PIL import Image
 
-image = Image.open(r'../Voice-Gender-Predictor/Picture needed/vecteezy_male-and-female-gender-icon-symbol-vector_7737986.jpg')
+image = Image.open(r'Picture needed/vecteezy_male-and-female-gender-icon-symbol-vector_7737986.jpg')
 
 st.image(image)
 # Créer des curseurs pour chaque variable
